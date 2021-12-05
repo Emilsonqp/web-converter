891fd2f7857f92e2cad8563cffeb5b02f4bdb2db
emilsonqp <e.quinterop@uniandes.edu.co>
2021-12-05 06:12:26 -0500

    fix

--- converter/server.py
+++ converter/server.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dqbxzwmkgoeqzp:b48d9b0f64812d3c53ed6bad86874b7c2f1309aaf97d4a1c40703cec29e92156@ec2-3-230-219-251.compute-1.amazonaws.com:5432/d2917gi1aubhfl'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hlfjayuvhpajqc:d79309f920e2f57d688fc6a415d60d81b603d00eaf1e5f5127722bf611988e24@ec2-3-230-219-251.compute-1.amazonaws.com:5432/do2vr4v7ha391'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = 'ROCK&ROLL_TRAIN_ACDC'
     app.config['PROPAGATE_EXCEPTION'] = True
