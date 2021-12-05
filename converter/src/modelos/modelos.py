dd93d5e87daa90deeedcb9f208368abd5250db13
emilsonqp <e.quinterop@uniandes.edu.co>
2021-12-05 06:51:25 -0500

    postgres

--- converter/src/modelos/modelos.py
+++ converter/src/modelos/modelos.py
@@ -13,7 +13,7 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50))
-    password1 = db.Column(db.String(50))
+    password1 = db.Column(db.String(100))
     email = db.Column(db.String(100))
     tasks = relationship('Task')
 
