57f60f95082dffcf3ecc0d316abb5b87e0f0ab9b
emilsonqp <e.quinterop@uniandes.edu.co>
2021-12-05 11:48:52 -0500

    ajuste files

--- converter/src/tareas/tareas.py
+++ converter/src/tareas/tareas.py
@@ -25,9 +25,9 @@ def process_uploaded_task(task: dict):
         file_list = filename.split('.')
         new_file_name = f'{file_list[0]}.{new_format}'
         print("pre conversion")
-        client.download_file(recordings_bucket,f'app/files/{task["filename"]}', f'app/files/{task["filename"]}')
-        new_path = f'app/files/{new_file_name}'
-        os.system(f'sudo ffmpeg -i app/files/{task["filename"]} {new_path}')
+        client.download_file(recordings_bucket,f'src/files/{task["filename"]}', f'src/files/{task["filename"]}')
+        new_path = f'src/files/{new_file_name}'
+        os.system(f'sudo ffmpeg -i src/files/{task["filename"]} {new_path}')
         client.upload_file(new_path,recordings_bucket,new_path)
         print("post conversion")
         task = Task.query.get(task['id'])
