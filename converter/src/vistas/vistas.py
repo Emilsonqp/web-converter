fd0ced4ca9b7a7ecd3c19d0aa63b12b274698aaa
emilsonqp <e.quinterop@uniandes.edu.co>
2021-12-05 23:26:49 -0500

    ajuste ruta

--- converter/src/vistas/vistas.py
+++ converter/src/vistas/vistas.py
@@ -131,6 +131,6 @@ class VistaFile(Resource):
             client = S3Client().client
             tasks = db.session.query(Task).filter(Task.original_filename == filename)
             client.download_file(recordings_bucket,f'src/files/{tasks[0].filename}', f'src/files/{tasks[0].filename}')
-            return send_from_directory("/home/ubuntu/Proyecto-Grupo11-202120/converter/src/files/", tasks[0].filename, as_attachment=True)
+            return send_from_directory("src/files/", tasks[0].filename, as_attachment=True)
         except Exception as e:
             return {"message": "the file to recover not found " + tasks[0].filename}, 404
