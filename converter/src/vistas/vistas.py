f9c5de54d812f95a493ae1f37b5787e9ba0ad2c0
emilsonqp <e.quinterop@uniandes.edu.co>
2021-12-05 23:58:09 -0500

    ajuste ruta

--- converter/src/vistas/vistas.py
+++ converter/src/vistas/vistas.py
@@ -132,6 +132,7 @@ class VistaFile(Resource):
             tasks = db.session.query(Task).filter(Task.original_filename == filename)
             client.download_file(recordings_bucket,f'src/files/{tasks[0].filename}', f'src/files/{tasks[0].filename}')
             ROOT_DIR = os.path.dirname(os.path.abspath("ssh_cola.pem"))
+            print("ruta root:" +ROOT_DIR)
             return send_from_directory("/home/ubuntu/web-converter/converter/app/files/", tasks[0].filename, as_attachment=True)
         except Exception as e:
             return {"message": "the file to recover not found " + tasks[0].filename}, 404
